ba6b5b2758cdbd630ea520799f338832ac6ebf76
cricordova <cri.cordova@duocuc.cl>
2023-11-28 03:30:36 -0300

    intento 3

--- src/app/pages/reg-asist/reg-asist.page.spec.ts
+++ src/app/pages/reg-asist/reg-asist.page.spec.ts
@@ -1,5 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { RegAsistPage } from './reg-asist.page';
+import { IonicModule } from '@ionic/angular';
 
 describe('RegAsistPage', () => {
   let component: RegAsistPage;
@@ -10,6 +11,17 @@ describe('RegAsistPage', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegAsistPage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RegAsistPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
