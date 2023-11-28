61549d0ea20e3856e42b39b8a30a86171d142a38
cricordova <cri.cordova@duocuc.cl>
2023-11-28 02:34:33 -0300

    intento 2

--- src/app/pages/reg-asist/reg-asist.page.ts
+++ src/app/pages/reg-asist/reg-asist.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Barcode, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-reg-asist',
@@ -8,13 +9,39 @@ import { Router } from '@angular/router';
 })
 export class RegAsistPage implements OnInit {
 
-  constructor(private router:Router) { }
+  isSupported = false;
+  barcodes: Barcode[] = [];
+
+  constructor(private alertController : AlertController) { }
 
   ngOnInit() {
+    BarcodeScanner.isSupported().then((result) =>{
+      this.isSupported = result.supported;
+    });
   }
 
-  irConfirmarAsistencia(){
-    this.router.navigate(['/confirmar-asistencia'])
+  async scan(): Promise<void> {
+    const granted = await this.requestPermissions();
+    if (!granted) {
+      this.presentAlert();
+      return;
+    }
+    const { barcodes } = await BarcodeScanner.scan();
+    this.barcodes.push(...barcodes);
+  }
+
+  async requestPermissions(): Promise<boolean> {
+    const { camera } = await BarcodeScanner.requestPermissions();
+    return camera === 'granted' || camera === 'limited';
+  }
+
+  async presentAlert(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Permission denied',
+      message: 'Please grant camera permission to use the barcode scanner.',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 
 }
